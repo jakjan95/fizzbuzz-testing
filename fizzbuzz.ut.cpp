29d57a32859f1ab1db8621a0885c0ee0a879f0d4
jakjan95 <jakubjanusz95@gmail.com>
2020-07-17 15:11:09 +0200

    negative numbers test

--- fizzbuzz.ut.cpp
+++ fizzbuzz.ut.cpp
@@ -43,3 +43,16 @@ TEST_CASE("Test positives", "[classic]") {
         }
     }
 }
+
+TEST_CASE("Test negatives", "[classic]"){
+        REQUIRE(fizzbuzz(-1) == "-1");
+        REQUIRE(fizzbuzz(-2) == "-2");
+        REQUIRE(fizzbuzz(-3) == "fizz");
+        REQUIRE(fizzbuzz(-4) == "-4");
+        REQUIRE(fizzbuzz(-5) == "buzz");
+        REQUIRE(fizzbuzz(-6) == "fizz");
+        REQUIRE(fizzbuzz(-7) == "-7");
+        REQUIRE(fizzbuzz(-8) == "-8");
+        REQUIRE(fizzbuzz(-9) == "fizz");
+        REQUIRE(fizzbuzz(-10) == "buzz");
+}
